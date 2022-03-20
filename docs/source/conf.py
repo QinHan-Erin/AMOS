3bade8daad1391ca46f6a51510558dd8f44aa223
QinHan-Erin <1900012949@pku.edu.cn>
2022-03-20 16:19:53 +0800

    modified:   source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
 curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
+sys.path.insert(0, os.path.join(curr_path, "../../python"))
 sys.path.insert(0, os.path.join(curr_path, "../../python/tvm/auto_tensorize"))
 sys.path.insert(0, os.path.join(curr_path, "../../python/tvm"))
 
